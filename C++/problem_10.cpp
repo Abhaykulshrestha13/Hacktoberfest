1d201898d21812c290f8b690851c86a631fd6946
keshav5548 <ksharma5_be20@thapar.edu>
2021-10-05 13:48:49 +0530

    Optimized solution of Q10

--- C++/problem_10.cpp
+++ C++/problem_10.cpp
@@ -1,37 +1,28 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
-
 int main()
 {
   int n=10;
   cin>>n;
-  int c=0;
-  
-  
-  for(int i=2;i<=n;i++)
+  int sum=0;
+  vector<int> arr;
+  arr.push_back(0);  
+  for(int i=1;i<=n;i++)   
   {
-      
-     int flag=0;
-      for(int j=2;j*j<=i;j++)
+      arr.push_back(i);
+  }
+   for(int i=2;i<=n;i++)   //Marking all non primes as 1 and adding all primes in sum using sieve
+  {
+
+      if(arr[i]!=1)
       {
-     
-          if((i%j)==0)
+          sum+=arr[i];
+          for(int j=i+i;j<=n;j+=i)
           {
-              flag=1;
-              break;
+              arr[j]=1;
           }
-          
       }
-      if(flag==0)
-      {
-       // cout<<i<<" ";
-          c+=i;
-      }
-      
-  
-   }
-  cout<<c;
-  
+  }
+  cout<<sum;
    return 0;
 }
